be6cbffb7d5b24529876c0a37105a9d6016f5f25
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-07-25 12:25:18 +0300

    the users router (v1)

--- app/routers/v1/users.py
+++ app/routers/v1/users.py
@@ -1,7 +1,11 @@
+from typing import List
+
 from app.common.v1.urls import USERS
+from app.data.v1.db_mock import users
 from app.main import app
+from app.model.v1.users.User import User
 
 
-@app.get(USERS)
+@app.get(USERS, response_model=List[User])
 async def get_users():
-    return None  # TODO:
+    return users
