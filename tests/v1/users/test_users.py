295bf74efa3edf5556c97a1814b87f5f459a94bd
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-05-08 11:26:25 +0300

    last changes

--- tests/v1/users/test_users.py
+++ tests/v1/users/test_users.py
@@ -10,12 +10,12 @@ client = TestClient(app1)
 
 def test_get_users():
     response = client.get(USERS)
-    assert OK == response.status_code
-    assert hubstaff_users == response.json()
+    assert response.status_code == OK
+    assert response.json() == hubstaff_users
 
 
 def test_get_user_by_id():
     response = client.get(USER.format(id=USER01_ID))
-    assert OK == response.status_code
-    assert [x for x in hubstaff_users
-            if x.id == USER01_ID][0] == response.json()
+    assert response.status_code == OK
+    assert response.json() == \
+           [x for x in hubstaff_users if x.id == USER01_ID][0]
